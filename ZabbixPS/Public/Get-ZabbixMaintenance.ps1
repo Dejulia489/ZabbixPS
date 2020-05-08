d3b5e840d5aea878d51125fde80bc1ede872bd85
Michael Dejulia <michael.dejulia@gmail.com>
2020-05-07 21:21:43 -0700

    Get, Remove Maint

--- ZabbixPS/Public/Get-ZabbixMaintenance.ps1
+++ ZabbixPS/Public/Get-ZabbixMaintenance.ps1
@@ -1,103 +1,147 @@
-
-Function Get-ZabbixMaintenance
+function Get-ZabbixMaintenance
 {
-	<# 
-	.Synopsis
-		Get maintenance
-	.Description
-		Get maintenance
-	.Parameter MaintenanceName
-		To filter by name of the maintenance (case sensitive)
-	.Parameter MaintenanceID
-		To filter by id of the maintenance
-	.Example
-		Get-ZabbixMaintenance | select maintenanceid,name
-		Get maintenance
-	.Example
-		Get-ZabbixMaintenance -MaintenanceName MaintenanceName
-		Get maintenance by name (case sensitive)
-	.Example 
-		Get-ZabbixMaintenance | ? name -match maintenance
-		Get maintenance by name match (case insensitive)
-    .Example
-        Get-ZabbixMaintenance | ? name -match "" | select @{n="MaintenanceName";e={$_.name}} -ExpandProperty groups | ft -a
-        Get maintenance by name match (case insensitive)   
-	.Example
-		Get-ZabbixMaintenance -MaintenanceID 10123
-		Get maintenance by ID
-	.Example
-        Get-ZabbixMaintenance | select maintenanceid,name,@{n="Active_since(UTC-5)";e={(convertFrom-epoch $_.active_since).addhours(-5)}},@{n="Active_till(UTC-5)";e={(convertFrom-epoch $_.active_till).addhours(-5)}},@{n="TimeperiodStart(UTC-5)";e={(convertfrom-epoch $_.timeperiods.start_date).addhours(-5)}},@{n="Duration(hours)";e={$_.timeperiods.period/3600}} | ft -a
-        Get maintenance and it's timeperiod
-	.Example
-		(Get-ZabbixMaintenance -MaintenanceName MaintenanceName).timeperiods
-		Get timeperiods from maintenance (case sensitive)
-    .Example
-        Get-ZabbixMaintenance | select -Property @{n="MaintenanceName";e={$_.name}} -ExpandProperty timeperiods | ft -a
-        Get timeperiods from maintenance
-	.Example
-        Get-ZabbixMaintenance | select -Property @{n="MaintenanceName";e={$_.name}} -ExpandProperty timeperiods | select MaintenanceName,timeperiodid,timeperiod_type,@{n="start_date(UTC)";e={convertfrom-epoch $_.start_date}},@{n="period(Hours)";e={$_.period/3600}} | ft -a
-        Get timeperiods maintenance and timeperiods (Time in UTC)
-    .Example
-		(Get-ZabbixMaintenance -MaintenanceName MaintenanceName).hosts.host
-		Get hosts from maintenance (case sensitive)
-	.Example
-		(Get-ZabbixMaintenance -MaintenanceName MaintenanceName).hostid  
-		Get HostIDs of hosts from maintenance (case sensitive)
-	.Example
-		Get-ZabbixMaintenance | ? name -match maintenance | select Name,@{n="TimeperiodStart";e={(convertfrom-epoch $_.timeperiods.start_date).addhours(-5)}},@{n="Duration(hours)";e={$_.timeperiods.period/3600}}
-		Get timeperiods from maintenance (case insensitive), display name, timeperiod (according UTC-5) and duration
-	#>
-    
-	[CmdletBinding()]
-	[Alias("gzm")]
-	Param (
-		$MaintenanceName,
-		$MaintenanceID,
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$jsonrpc = ($global:zabSessionParams.jsonrpc),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$session = ($global:zabSessionParams.session),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$id = ($global:zabSessionParams.id),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$URL = ($global:zabSessionParams.url)
-	)
-    
-	process
- {
+    <#
+    .SYNOPSIS
 
-		if (!(Get-ZabbixSession)) { return }
+    Gets a Zabbix maintenance period.
 
-		$boundparams = $PSBoundParameters | out-string
-		write-verbose "($boundparams)"
-		
-		$Body = @{
-			method  = "maintenance.get"
-			params  = @{
-				output            = "extend"
-				selectGroups      = "extend"
-				selectHosts       = "extend"
-				selectTimeperiods = "extend"
-				filter            = @{
-					name = $MaintenanceName
-				}
-				maintenanceids    = $MaintenanceID
-			}
+    .DESCRIPTION
 
-			jsonrpc = $jsonrpc
-			id      = $id
-			auth    = $session
-		}
+    Gets a Zabbix maintenance period.
 
-		$BodyJSON = ConvertTo-Json $Body
-		write-verbose $BodyJSON
-		
-		try
-		{
-			$a = Invoke-RestMethod "$URL/api_jsonrpc.php" -ContentType "application/json" -Body $BodyJSON -Method Post
-			if ($a.result) { $a.result } else { $a.error }
-		}
-		catch
-		{
-			Write-Host "$_"
-			Write-Host "Too many entries to return from Zabbix server. Check/reduce the filters." -f cyan
-		}
-	}
+    .PARAMETER Uri
+
+    The Zabbix instance uri.
+
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to the project.
+
+    .PARAMETER Proxy
+
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    ZabbixPS session, created by New-ZabbixSession.
+
+    .PARAMETER Name
+
+    The maintenance name.
+
+	.PARAMETER Id
+
+	The maintenance id.
+
+    .INPUTS
+
+    None, does not support pipeline.
+
+    .OUTPUTS
+
+    None, does not support output.
+
+    .EXAMPLE
+
+    .LINK
+
+    https://www.zabbix.com/documentation/4.2/manual/api/reference/maintenance/delete
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByCredential')]
+    param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Uri,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter()]
+        [string]
+        $Name,
+
+        [Parameter()]
+        [int]
+        $Id
+    )
+
+    begin
+    {
+        if ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-ZabbixSession
+            if ($currentSession)
+            {
+                $Uri = $currentSession.Uri
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                $ApiVersion = $currentSession.ApiVersion
+            }
+        }
+    }
+
+    process
+    {
+        $body = @{
+            method  = 'maintenance.get'
+            jsonrpc = $ApiVersion
+            id      = 1
+
+            params  = @{
+                output            = "extend"
+                selectGroups      = "extend"
+                selectHosts       = "extend"
+                selectTimeperiods = "extend"
+                filter            = @{
+                    name = $Name
+                }
+                maintenanceids    = $Id
+            }
+        }
+        $invokeZabbixRestMethodSplat = @{
+            Body        = $body
+            Uri         = $Uri
+            Credential  = $Credential
+            ApiVersion  = $ApiVersion
+            ErrorAction = 'Stop'
+        }
+        if ($Proxy)
+        {
+            $invokeZabbixRestMethodSplat.Proxy = $Proxy
+            if ($ProxyCredential)
+            {
+                $invokeZabbixRestMethodSplat.ProxyCredential = $ProxyCredential
+            }
+            else
+            {
+                $invokeZabbixRestMethodSplat.ProxyUseDefaultCredentials = $true
+            }
+        }
+        return Invoke-ZabbixRestMethod @invokeZabbixRestMethodSplat
+    }
+
+    end
+    {
+    }
 }
-
--- ZabbixPS/Public/Remove-ZabbixMaintenance.ps1
+++ ZabbixPS/Public/Remove-ZabbixMaintenance.ps1
@@ -1,66 +1,130 @@
-Function Remove-ZabbixMaintenance
+function Remove-ZabbixMaintenance
 {
-	<# 
-	.Synopsis
-		Remove maintenance
-	.Description
-		Remove maintenance
-	.Parameter MaintenanceID
-		To filter by ID/IDs of the maintenance
-	.Example
-		Remove-ZabbixMaintenance -MaintenanceID "3","4" 
-		Remove maintenance by IDs
-	.Example
-		Remove-ZabbixMaintenance -MaintenanceID (Get-ZabbixMaintenance | ? name -match "Maintenance|Name").maintenanceid -WhatIf
-		Remove multiple maintenances (check only: -WhatIf)
-    .Example
-		Remove-ZabbixMaintenance -MaintenanceID (Get-ZabbixMaintenance | ? name -match "Maintenance|Name").maintenanceid
-		Remove multiple maintenances
-	.Example
-		Get-ZabbixMaintenance | ? name -eq name | Remove-ZabbixMaintenance
-		Remove single maintenance by name
-	#>
-    
-	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
-	[Alias("rzm")]
-	Param (
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][array]$Name,
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][array]$MaintenanceID,
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$jsonrpc = ($global:zabSessionParams.jsonrpc),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$session = ($global:zabSessionParams.session),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$id = ($global:zabSessionParams.id),
-		[Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $true)][string]$URL = ($global:zabSessionParams.url)
-	)
-     
-	process
- {
+    <#
+    .SYNOPSIS
 
-		if (!(Get-ZabbixSession)) { return }
-		elseif (!$psboundparameters.count) { Write-MissingParamsMessage; return }
+    Removes a Zabbix maintenance period.
 
-		$boundparams = $PSBoundParameters | out-string
-		write-verbose "($boundparams)"
-		
-		$Body = @{
-			method  = "maintenance.delete"
-			params  = @($MaintenanceID)
-			
-			jsonrpc = $jsonrpc
-			id      = $id
-			auth    = $session
-		}
-		
-		$BodyJSON = ConvertTo-Json $Body
-		write-verbose $BodyJSON
-		
-		
-		if ([bool]$WhatIfPreference.IsPresent) { }
-		if ($PSCmdlet.ShouldProcess($Name, "Delete"))
-		{  
-			$a = Invoke-RestMethod "$URL/api_jsonrpc.php" -ContentType "application/json" -Body $BodyJSON -Method Post
-		}
-		
-		if ($a.result) { $a.result } else { $a.error }
-	}
+    .DESCRIPTION
+
+    Removes a Zabbix maintenance period.
+
+    .PARAMETER Uri
+
+    The Zabbix instance uri.
+
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to the project.
+
+    .PARAMETER Proxy
+
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    ZabbixPS session, created by New-ZabbixSession.
+
+	.PARAMETER Id
+
+	The maintenance id.
+
+    .INPUTS
+
+    None, does not support pipeline.
+
+    .OUTPUTS
+
+    None, does not support output.
+
+    .EXAMPLE
+
+    .LINK
+
+    https://www.zabbix.com/documentation/4.2/manual/api/reference/maintenance/delete
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByCredential')]
+    param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Uri,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [int]
+        $Id
+    )
+
+    begin
+    {
+        if ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-ZabbixSession
+            if ($currentSession)
+            {
+                $Uri = $currentSession.Uri
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                $ApiVersion = $currentSession.ApiVersion
+            }
+        }
+    }
+
+    process
+    {
+        $body = @{
+            method  = 'maintenance.delete'
+            jsonrpc = $ApiVersion
+            id      = 1
+
+            params  = @($Id)
+        }
+        $invokeZabbixRestMethodSplat = @{
+            Body        = $body
+            Uri         = $Uri
+            Credential  = $Credential
+            ApiVersion  = $ApiVersion
+            ErrorAction = 'Stop'
+        }
+        if ($Proxy)
+        {
+            $invokeZabbixRestMethodSplat.Proxy = $Proxy
+            if ($ProxyCredential)
+            {
+                $invokeZabbixRestMethodSplat.ProxyCredential = $ProxyCredential
+            }
+            else
+            {
+                $invokeZabbixRestMethodSplat.ProxyUseDefaultCredentials = $true
+            }
+        }
+        return Invoke-ZabbixRestMethod @invokeZabbixRestMethodSplat
+    }
+
+    end
+    {
+    }
 }
-
